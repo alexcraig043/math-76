2ee8ff3546737dfb2a6206fbceb8ac6c7d40c186
alexcraig <alexcraig043@gmail.com>
2024-04-17 17:36:50 -0400

    switched dimensionality

--- python/enkf.py
+++ python/enkf.py
@@ -4,8 +4,8 @@ import numpy as np
 class EnKF:
     def __init__(
         self,
-        F,  # A function that propagates the state forward
-        H,  # A matrix that maps the true state space to the observed space
+        F,  # A function that propagates the state forward (n x 1 -> n x 1)
+        H,  # A matrix that maps the true state space to the observed space (n x m)
         Qsqrt,  # The square root of the state noise covariance matrix
         Rsqrt,  # The square root of the observation noise covariance matrix
         seed,  # Seed for random number generator
@@ -17,8 +17,8 @@ class EnKF:
         self.H = H
         self.Qsqrt = Qsqrt  # State noise (also Sigma in notation)
         self.Rsqrt = Rsqrt  # Measurement noise (also Gamma in notation)
-        self.n = self.H.shape[1]  # Infer state space dimension
-        self.m = self.H.shape[0]  # Measurement space dimension
+        self.n = self.H.shape[0]  # Infer state space dimension
+        self.m = self.H.shape[1]  # Measurement space dimension
         self.n_members = n_members
 
         if seed is not None:
@@ -33,13 +33,13 @@ class EnKF:
         # Handle ensemble initialization
         if (initialization is not None) and (n_members is not None):
             assert initialization.shape == (
-                self.n_members,
                 self.n,
+                self.n_members,
             ), "invalid shape for initialization."
             self.members = initialization
         elif (initialization is None) and (n_members is not None):
             # Give random intialization
-            self.members = np.random.normal(size=(self.n_members, self.n))
+            self.members = np.random.normal(size=(self.n, self.n_members))
         else:
             raise ValueError("Must give n_members!")
 
@@ -65,19 +65,19 @@ class EnKF:
             # Apply the forward model to the ensemble member
             if self.with_state_noise:
                 # With state noise
-                self.members_pred[j, :] = self.F(self.members[j, :]) + (
+                self.members_pred[:, j] = self.F(self.members[:, j]) + (
                     self.Qsqrt @ np.random.normal(size=self.n)
                 )
             else:
                 # Without state noise
-                self.members_pred[j, :] = self.F(self.members[j, :])
+                self.members_pred[:, j] = self.F(self.members[:, j])
 
         # Get the mean prediction of the forecasted ensemble
         # Note: taking the average of each state entry across all members
-        self.mean_pred = np.mean(self.members_pred, axis=0)
+        self.mean_pred = np.mean(self.members_pred, axis=1)
 
-        # Get the covariance of the forecasted ensemble (each row is a state variable, so we need to transpose the matrix)
-        self.cov_pred = np.cov(self.members_pred.T)
+        # Get the covariance of the forecasted ensemble
+        self.cov_pred = np.cov(self.members_pred)
 
         return None
 
@@ -85,11 +85,11 @@ class EnKF:
         """Carries out the analysis step."""
 
         # Generate the blank analysis ensemble
-        v_perturbed = np.zeros((self.n_members, self.m))
+        v_perturbed = np.zeros((self.m, self.n_members))
 
         # For each ensemble member, generate a perturbed observation
         for j in range(self.n_members):
-            v_perturbed[j, :] = v + (self.Rsqrt @ np.random.normal(size=self.m))
+            v_perturbed[:, j] = v + (self.Rsqrt @ np.random.normal(size=self.m))
 
         # Define the matrix that needs to be inverted
         # Note: self.Rsqrt @ self.Rsqrt.T generates the observation noise covariance matrix
@@ -104,15 +104,15 @@ class EnKF:
             K = self.cov_pred @ self.H.T @ B_inv
 
             # Compute the analysis state (u_new = u + K(y - H u))
-            new_state = self.members_pred[j, :] + K @ (
-                v_perturbed[j, :] - self.H @ self.members_pred[j, :]
+            new_state = self.members_pred[:, j] + K @ (
+                v_perturbed[:, j] - self.H @ self.members_pred[:, j]
             )
 
             # Update the ensemble member
-            self.members[j, :] = new_state
+            self.members[:, j] = new_state
 
         # Update the mean and covariance of the analysis ensemble
-        self.mean = np.mean(self.members_pred, axis=0)
-        self.cov = np.cov(self.members_pred.T)
+        self.mean = np.mean(self.members_pred, axis=1)
+        self.cov = np.cov(self.members_pred)
 
         return None
